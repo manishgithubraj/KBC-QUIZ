a27dcc8b106184ff53c8ab7cbdcfd813446a01b5
Manish Raj <125475647+manishgithubraj@users.noreply.github.com>
2023-03-28 23:35:32 +0530

    Add files via upload
    initial commit

---
+++ KBC.py
@@ -0,0 +1,173 @@
+# This is the first kbc question
+question1 = ["who is the best coder of india ?"]
+options = [" 1. CodeWithHarry "]
+print(tuple(question1))
+print(tuple(options))
+options = [" 2. CodeNinja "]
+print(tuple(options))
+options = [" 3. Satya Nadella "]
+print(tuple(options))
+options = [" 4. Kris Gopalakrishnan "]
+print(tuple(options))
+x = int(input("write the option number u think the right is:\n"))
+
+if (x == 1):
+  print("yes the answer is correct and you won 1000 rupees!!")
+
+elif (x == 2):
+  print("sorry,you lost!!")
+
+elif (x == 3):
+  print("sorry,you lost!!")
+elif (x == 4):
+  print("sorry,you lost!!")
+
+else:
+  print("sorry you printed the wrong input")
+
+print(
+  "the right answer of the previous question is 'CodeWithHarry',if you were right kindly ignore this\n"
+)
+
+print("lets come back for the next question\n")
+
+# This is the second kbc question
+
+question2 = ["Which of the following is not a programming language?"]
+options = [" 1. java "]
+print(tuple(question2))
+print(tuple(options))
+options = [" 2. python "]
+print(tuple(options))
+options = [" 3.c++"]
+print(tuple(options))
+options = [" 4. photoshop "]
+print(tuple(options))
+x = int(input("write the option number u think the right is:\n"))
+
+if (x == 4):
+  print("yes the answer is correct and you won 2000 rupees!!")
+
+elif (x == 2):
+  print("sorry,you lost!!")
+
+elif (x == 3):
+  print("sorry,you lost!!")
+elif (x == 1):
+  print("sorry,you lost!!")
+
+else:
+  print("sorry you printed the wrong input")
+
+print(
+  "the right answer of the previous question is 'photoshop',if you were right kindly ignore this\n"
+)
+
+print("so this is the third question\n")
+# This is the third kbc question
+
+question3 = [
+  '''What is the output of the following code in Python?   x = 5  y = 3  print(x % y)'''
+]
+options = [" a) 1 "]
+print(tuple(question3))
+print(tuple(options))
+options = [" b) 2 "]
+print(tuple(options))
+options = [" c) 3 "]
+print(tuple(options))
+options = [" d) 4 "]
+print(tuple(options))
+x = str(input("write the option value (a,b,c,d) u think the right is:\n"))
+
+if (x == "b"):
+  print("yes the answer is correct and you won 5000 rupees!!")
+
+elif (x == "a"):
+  print("sorry,you lost!!")
+
+elif (x == "c"):
+  print("sorry,you lost!!")
+elif (x == "d"):
+  print("sorry,you lost!!")
+
+else:
+  print("sorry you printed the wrong input")
+
+print(
+  "the right answer of the previous question is 'option (b)',if you were right kindly ignore this\n"
+)
+
+print("this is the fourth question\n")
+
+# this is the fourth question
+
+question4 = [
+  "Which of the following is an example of a front-end web development framework?"
+]
+options = [" a) Django "]
+print(tuple(question4))
+print(tuple(options))
+options = [" b) Ruby on Rails "]
+print(tuple(options))
+options = [" c) AngularJS "]
+print(tuple(options))
+options = [" d) Node.js "]
+print(tuple(options))
+x = str(input("write the option value (a,b,c,d) u think the right is:\n"))
+
+if (x == "c"):
+  print("yes the answer is correct and you won 10000 rupees!!")
+
+elif (x == "a"):
+  print("sorry,you lost!!")
+
+elif (x == "b"):
+  print("sorry,you lost!!")
+elif (x == "d"):
+  print("sorry,you lost!!")
+
+else:
+  print("sorry you printed the wrong input")
+
+print(
+  "the right answer of the previous question is 'option (c)',if you were right kindly ignore this\n"
+)
+
+print("this is the fifth and last  question\n")
+
+# this is the fifth and last  question
+
+question5 = ["What does CSS stand for?"]
+options = [" a)  Cascading Script Style "]
+print(tuple(question3))
+print(tuple(options))
+options = [" b) Cascading Style Sheets "]
+print(tuple(options))
+options = [" c) Cascading System Sheets "]
+print(tuple(options))
+options = [" d) Cascading Style Scripts "]
+print(tuple(options))
+x = str(input("write the option value (a,b,c,d) u think the right is:\n"))
+
+if (x == "b"):
+  print("yes the answer is correct and you won 20000 rupees!!")
+
+elif (x == "a"):
+  print("sorry,you lost!!")
+
+elif (x == "c"):
+  print("sorry,you lost!!")
+elif (x == "d"):
+  print("sorry,you lost!!")
+
+else:
+  print("sorry you printed the wrong input")
+
+print(
+  "the right answer of the previous question is 'option (b)',if you were right kindly ignore this\n"
+)
+
+print(
+  "Good job You completed your text, go and check whether you performed well or not...GOOD LUCK !!"
+)
